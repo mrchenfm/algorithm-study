67075c1cffb619dee7a9f68e1a506dcbc925050a
mrchenfm <1499967901@qq.com>
2021-03-15 22:59:59 +0800

    初始化

---
+++ src/main/java/com/cfm/algorithm/day1/Test.java
@@ -0,0 +1,19 @@
+package com.cfm.algorithm.day1;
+
+/**
+ * @version v1.0
+ * @ProjectName: algorithm-study
+ * @ClassName: Test
+ * @Description: TODO(一句话描述该类的功能)
+ * @Author: fangming_chen
+ * @Date: 2021/03/15 21:41
+ */
+public class Test {
+
+	public static void main(String[] args) {
+		int[][] arr = new int[6][7];
+
+		int a= arr.length;
+		int b= arr[0].length;
+	}
+}
